cf5cef1d2203f9f295c9efb0a2e9512233808904
Stephanie Liu <yliu@Stephanies-MacBook-Air.local>
2021-08-03 09:07:53 -0500

    data

--- src/components/ExhibitionList.js
+++ src/components/ExhibitionList.js
@@ -1,22 +1,47 @@
 import React from 'react'
-import {
-    Card, CardImg, CardBody, CardLink,
-    CardTitle, CardSubtitle, Button
-  } from 'reactstrap';
+// import {
+//     Card, CardImg, CardBody, CardLink,
+//     CardTitle, CardSubtitle, Button
+//   } from 'reactstrap';
   import { BsFillHeartFill } from "react-icons/bs";
   
+
 const ExhibitionList = (props) => {
-      return(
-        // {props.exhibit.map((exhibit,index) => {
-       <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">CardTitle</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardLink href="#">Card Link</CardLink>
-          <Button><BsFillHeartFill/></Button>
-        </CardBody>
-      </Card>
-      )}
+  // console.log("Products", props);
+  const loaded = () => {
+    return (
+      <div className="container">
+        {props.exhibit.map((exhibit, index) => {
+          return (
+              <div className="exhibit-card" key={index}>
+             <img src={exhibit.image} alt=""></img>
+              <h1 className="name">{exhibit.title}</h1>
+              <p className="date">{exhibit.startDate} - {exhibit.endDate}</p>
+              <a href={exhibit.link}>See More</a>
+              {/* <div className="link">{exhibit.link}</div> */}
+              <button className="like"><BsFillHeartFill/></button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+  const loading = () => {
+    return <h1>No List to Display</h1>;
+  };
+
+  return props ? loaded() : loading();
+};
+
+    //    <Card>
+    //     <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+    //     <CardBody>
+    //       <CardTitle tag="h5">CardTitle</CardTitle>
+    //       <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+    //       <CardLink href="#">Card Link</CardLink>
+    //       <Button><BsFillHeartFill/></Button>
+    //     </CardBody>
+    //   </Card>
+    
     
 export default ExhibitionList
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,11 +15,11 @@ const Footer = () => {
         alt="Aic-location"
         />
         <p>Michigan Avenue Entrance<br/>
-        111 South Michigan Avenue
-        Chicago, IL 60603</p>
+        <a href ="https://www.google.com/maps/place/The+Art+Institute+of+Chicago/@41.8795845,-87.625902,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m5!3m4!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!8m2!3d41.8795847!4d-87.623713 ">111 South Michigan Avenue
+        Chicago, IL 60603</a></p>
         <p> Modern Wing Entrance <br/>
-        159 East Monroe Street
-         Chicago, IL 60603</p>
+        <a href ="https://www.google.com/maps/place/Modern+Wing+Entrance/@41.8797032,-87.6244036,17z/data=!3m1!5s0x880e2ca148f260e3:0xd473c3802aaff420!4m12!1m6!3m5!1s0x880e2ca3e2d94695:0x4829f3cc9ca2d0de!2sThe+Art+Institute+of+Chicago!8m2!3d41.8795847!4d-87.623713!3m4!1s0x880e2ca6cf1a0e41:0xa97b8d9d0c91fa6e!8m2!3d41.880725!4d-87.621932 ">159 East Monroe Street
+         Chicago, IL 60603</a></p>
         </div>
     )
 }
